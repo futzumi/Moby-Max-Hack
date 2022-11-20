9729e41e2f15a4cd352a6be534cc2355a246a570
Overlord <102948087+VirescentOverlord@users.noreply.github.com>
2022-11-19 23:08:25 -0500

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,9 @@
-javascript:req = new XMLHttpRequest();
+javascript:
+try { req = new XMLHttpRequest();
 req.open('GET', 'https://cdn.jsdelivr.net/gh/VirescentOverlord/Moby-Max-Hack@main/Main%20Script');
 req.onload = function() {
   eval(this.responseText + 'm.appendChild(r)');
 };
 req.send();
+   } 
+catch(err) {alert(err)}
